3696471b778144008de65b75b31749d8d336ca12
uniblab <uniblab@hotmail.com>
2023-08-10 00:36:10 -0400

    fix: correct copyright

--- src/Program.cs
+++ src/Program.cs
@@ -108,26 +108,21 @@ namespace Icod.Inflate {
 		private static void PrintCopyright() {
 			var copy = new System.String[] {
 				"Gunzip.exe decompresses files using the Deflate compression algorithm.",
-				"",
 				"Copyright( C ) 2023 Timothy J. Bruce",
+				"",
 				"",
 				"This program is free software: you can redistribute it and / or modify",
 				"it under the terms of the GNU General Public License as published by",
 				"the Free Software Foundation, either version 3 of the License, or",
 				"( at your option ) any later version.",
 				"",
-				"",
 				"This program is distributed in the hope that it will be useful,",
 				"but WITHOUT ANY WARRANTY; without even the implied warranty of",
-				"",
 				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the",
-				"",
 				"GNU General Public License for more details.",
 				"",
-				"",
 				"You should have received a copy of the GNU General Public License",
-				"",
-				"along with this program.If not, see < https://www.gnu.org/licenses/>."
+				"along with this program.If not, see <https://www.gnu.org/licenses/>."
 			};
 			foreach ( var line in copy) {
 				System.Console.WriteLine( line );
